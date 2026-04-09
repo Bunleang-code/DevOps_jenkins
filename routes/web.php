5f2bfd3c1f666dee239f252dd4c84e4394caa04a
Bunleang-code <lobunleang@gmail.com>
2026-04-09 20:48:45 +0700

    a webpage

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Portfolio')->name('home');
+Route::inertia('/', 'Welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+});
 
 require __DIR__.'/settings.php';
