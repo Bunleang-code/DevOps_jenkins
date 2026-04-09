bfaeabbf07cd65ad9ba7a16730f49c8dfaeab678
Bunleang-code <lobunleang@gmail.com>
2026-04-09 20:34:48 +0700

    default page

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::inertia('portfolio', 'Portfolio')->name('portfolio');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-});
+Route::inertia('/', 'Portfolio')->name('home');
 
 require __DIR__.'/settings.php';
