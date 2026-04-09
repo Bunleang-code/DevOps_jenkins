7ac52175a5393d2ad7a09f754e707e6a77f49b4c
Bunleang-code <lobunleang@gmail.com>
2026-04-09 19:57:40 +0700

    make a portfolio

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -45,6 +45,12 @@ withDefaults(
                     >
                         Register
                     </Link>
+                    <Link
+                        href="/portfolio"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    >
+                        Portfolio
+                    </Link>
                 </template>
             </nav>
         </header>
